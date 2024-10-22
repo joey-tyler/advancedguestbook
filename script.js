09c5e413fbbecd7760e7fe6de0342b1c5b5eef7f
joey-tyler <jtseewald@icloud.com>
2024-10-22 11:24:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 document.getElementById('guestbook-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-
-    const name = document.getElementById('name').value;
-    const message = document.getElementById('message').value;
-
-    // Check ob Feld auch ausgefüllt ist
+const name = document.getElementById('name').value;
+const message = document.getElementById('message').value;
+ // Check ob Feld auch ausgefüllt ist
     if (name && message) {
-        // ein Listenelement erstellen
+// ein Listenelement erstellen
         const li = document.createElement('li');
         li.textContent = `${name} sagt: ${message}`;
-
-        // Stern-Icon Hinzufügen 
+// Stern-Icon Hinzufügen 
         const starButton = document.createElement('button');
         starButton.innerHTML = '⭐';
         starButton.classList.add('star-button');
-
-        // Event-Listener hervorheben
+ // Event-Listener hervorheben
         starButton.addEventListener('click', function() {
             if (li.classList.contains('starred')) {
                 li.classList.remove('starred');  // Stern entfernen
@@ -25,10 +21,7 @@ document.getElementById('guestbook-form').addEventListener('submit', function(ev
             }
         });
 
-        
         li.appendChild(starButton);
-
-     
         document.getElementById('guestbook-entries').appendChild(li);
 
         
